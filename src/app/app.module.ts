3833c96da3dd2e156e9ac091cc2078d3797cc964
MASSOUD <Massoud.fradi@esprit.tn>
2024-03-11 18:54:33 +0000

    commit with massoud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatRippleModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './FrontOffice/header/header.component';
 import { FootorComponent } from './FrontOffice/footor/footor.component';
@@ -9,24 +33,80 @@ import { AppRoutingModule } from './app.routing.module';
 import { FooterBackComponent } from './BackOffice/footer-back/footer-back.component';
 import { TemplateBackComponent } from './BackOffice/template-back/template-back.component';
 import { HeadBackComponent } from './BackOffice/head-back/head-back.component';
-
-
+import { LoginComponent } from './Components/Pages/User/login/login.component';
+import { SignupComponent } from './Components/Pages/User/signup/signup.component';
+import { ForgotPasswordComponent } from './Components/Pages/User/forgot-password/forgot-password.component';
+import { ProfileComponent } from './Components/Pages/User/profile/profile.component';
+import { UpdateUserInfoComponentComponent } from './Components/Pages/User/update-user-info-component/update-user-info-component.component';
+import { DeleteUserComponent } from './Components/Pages/User/delete-user/delete-user.component';
+import { UpdatePhotoProfileComponent } from './Components/Pages/User/update-photo-profile/update-photo-profile.component';
+import { UpdateCoverPhotoProfileComponent } from './Components/Pages/User/update-cover-photo-profile/update-cover-photo-profile.component';
+import { UpdatePasswordComponent } from './Components/Pages/User/update-password/update-password.component';
+import { ShowPhotoProfileComponent  } from './Components/Pages/User/show-photo-profile/show-photo-profile.component';
+import { PhotoUploadDialogComponent } from './Components/Pages/User/photo-upload-dialog/photo-upload-dialog.component';
+import { ViewPhotoDialogComponent } from './Components/Pages/User/view-photo-dialog/view-photo-dialog.component';
+import { TokenInterceptor } from './token.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FootorComponent,
     TemplateFrontComponent,
-FooterBackComponent,
-TemplateBackComponent,
-HeadBackComponent
-    
+    FooterBackComponent,
+    TemplateBackComponent,
+    HeadBackComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    ProfileComponent,
+    UpdateUserInfoComponentComponent,
+    DeleteUserComponent,
+    UpdatePhotoProfileComponent,
+    UpdateCoverPhotoProfileComponent,
+    UpdatePasswordComponent,
+    ShowPhotoProfileComponent,
+    PhotoUploadDialogComponent,
+    ViewPhotoDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatDividerModule,
+    MatListModule,
+    MatTooltipModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatRippleModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
   ],
-  providers: [],
+  providers: [
+    // Fournir le TokenInterceptor en tant que fournisseur pour les intercepteurs HTTP
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true // Indique que cet interceptor est multi-instance, nécessaire pour HTTP_INTERCEPTORS
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
